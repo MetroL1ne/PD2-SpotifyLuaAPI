25201480bb2d20af05e3e2506d40d4dd4dcd67fe
MetroLine <73471643+MetroL1ne@users.noreply.github.com>
2025-08-16 21:19:04 +0800

    Delete lua directory

--- lua/localizations.lua
+++
@@ -1,7 +0,0 @@
-Hooks:Add("LocalizationManagerPostInit", "MenuManagerInitialize_RichPresenceMusical", function(loc)
-	rpm_languageList = {
-		"loc/english.txt"
-	}
-	rpm_languagePath = rpm_languageList[1]
-	loc:load_localization_file(RichPresenceMusical.path .. rpm_languagePath)
-end)
